da7f516df7440399c3175dc8bd86602bf9a0d117
tiger <vvp.vovarulit@gmail.com>
2019-06-24 23:42:48 +0700

    add message receiver func

--- tg.py
+++ tg.py
@@ -2,23 +2,40 @@ from telethon import TelegramClient, sync, events
 from telethon.tl.types import User
 import logging
 from id_hash import info
+#import socks
 
 id_ , h = info.info_() #get your app's id and hash on core.telegram.org 
-client = TelegramClient('first_attempt', id_, h)
+client = TelegramClient('first_attempt', id_, h) #, proxy=(socks.SOCKS4, '198.50.177.44', 44699) )
 
-class dialog_():
-	#all dialogs will be instances of this class
-	#TODO establish a faster connection to get messages faster 
+class dialog_():	
 	def __init__(self, dia_obj):
 		self.obj = 	dia_obj
 		self.id_ = dia_obj.id
 		self.hash = dia_obj.entity.access_hash
+		self.name = self.obj.name
 		self.messages = [] 
-		for message in client.iter_messages(self.obj, limit = 10):
+		self.senders = []
+		user = client.get_me()
+		for message in client.iter_messages(self.obj, limit = 10):	
 			self.messages.append(message.message)
+			if self.obj.id != client.get_me().id:
+				if message.sender.last_name: 
+					self.senders.append(str(message.sender.first_name + ' ' + message.sender.last_name))
+				else:
+					self.senders.append(message.sender.first_name)
+			else:
+				if user.last_name:
+					self.senders.append(user.first_name + ' ' + user.last_name)
+				else:
+					self.senders.append(user.first.name)		
+		self.messages.reverse()
+		if self.obj.id != client.get_me().id:
+			self.senders.reverse()
 
 	def __repr__(self):
-		return "id {} hash {} messages {}".format(self.id_, self.hash, self.messages)
+		return "id {} hash {} messages {} senders{}".format(self.id_, self.hash, self.messages, self.senders)
+ 
+
 
 	#def get_messages(self):
 	#	TODO this func is going to be used for getting messages from tg dialog so it can be placed in the app template
@@ -27,23 +44,36 @@ class dialog_():
 	#	TODO create a func for sending message 
 	
 
-
+#with method
 @client.on(events.NewMessage)
-async def my_event_handler(event):  
+async def my_event_handler(event):
+	chat_user_id = event.message.to_id.user_id
+	endpoint_id = None
 	sender = await event.get_sender()
+	if chat_user_id == dia[-1].id_:
+		endpoint_id = sender.id
+	else : 
+		endpoint_id = chat_user_id
+	i = 0
+
 	for obj in dia:
 		# TODO adapt the whole func for db instead of class' list 
-		if obj.id_ == sender.id and sender.id!=dia[-1].id_:
+		
+		if obj.id_ == endpoint_id:# and sender.id!=dia[-1].id_:
 			obj.messages.append(event.message.message)
-			
-			
+			kek = obj.messages
+			rofl = i 
+		i+=1
+	try:
+		print(dia[-1].messages)		
+	except:
+		pass		
 		
 
 def start(client):	
-	client.start()	
+	client.start(max_attempts = 1)
 	logging.basicConfig(level=logging.ERROR)
 	dia = getting_data(client)
-	print(dia[-1])
 	return dia
 
 def getting_data(client):
@@ -51,9 +81,9 @@ def getting_data(client):
 	dia = []
 	for ent in dialogs:
 		try:
-			if type(ent.entity)==User:
-				if ent.id == client.get_me().id:
-					myself = dialog_(dia_obj =ent)
+			if type(ent.entity) == User:
+				if ent.id == client.get_me().id:					
+					myself = dialog_(dia_obj = ent)
 				else:
 					obj = dialog_(dia_obj = ent )
 					dia.append(obj)		
@@ -64,10 +94,15 @@ def getting_data(client):
 	
 
 
-if __name__ == '__main__':		
-	dia =start(client)
+if __name__ == '__main__':			
+	dia =start(client)	
+	print(dia[-1].id_)
 	#TODO create a disconnect func	
-	client.run_until_disconnected()
+	client.disconnect()
+	print('done')
+	
+
+	#print(dir(client.iter_messages(dia[0].obj, limit=1)))
 	
 		
 
